48fd3341f73666b909149ed0647988a2c58c5af4
Xavian Moody <XavianMoody1025@gmail.com>
2025-02-01 14:23:43 -0500

    Created home page

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
+
+export const Home = () => {
+  return (
+    <main>
+      <section className=" h-screen flex flex-col gap-10 items-center justify-center">
+        <div className=" w-[80%] max-w-[600px]">
+          <img src={logo} alt="" className=" mx-auto w-full" />
+        </div>
+
+        <div className=" space-x-3">
+          <Link
+            to={"/movies"}
+            className=" border-2 bg-black text-white px-7 py-3 rounded-lg font-medium hover:bg-white hover:text-black duration-150"
+          >
+            Movies
+          </Link>
+
+          <Link
+            to={"/tv_shows"}
+            className=" border-2 bg-black text-white px-7 py-3 rounded-lg font-medium hover:bg-white hover:text-black duration-150"
+          >
+            TV Shows
+          </Link>
+        </div>
+      </section>
+    </main>
+  );
+};
