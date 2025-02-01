a05efc524177218b50cccf8329e1023138bf06f7
Xavian Moody <XavianMoody1025@gmail.com>
2025-02-01 14:05:30 -0500

    Project setup

---
+++ client/src/App.test.jsx
@@ -0,0 +1,7 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("should get hello world", () => {
+  render(<App></App>);
+  screen.getByText("Hello world!");
+});
